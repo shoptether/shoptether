4f64e8e3cfc503c68467da1fd440db716ab7163b
farahrobleh <farahrobleh1@gmail.com>
2024-11-17 14:33:59 -0800

    separate metadata from layout tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,32 +5,9 @@ import { Inter } from 'next/font/google'
 import { Navbar } from '@/components/layout/Navbar'
 import { Toaster } from "react-hot-toast"
 import { usePathname } from 'next/navigation'
-import type { Metadata } from "next"
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
-
-export const metadata: Metadata = {
-  title: 'ShopTether - AI-Powered Shopify Services',
-  description: 'Enhance your Shopify store with cutting-edge AI services for personalization, automation, and optimization.',
-  icons: {
-    icon: [
-      {
-        url: '/images/shop-tether-official-logo.jpg',
-        sizes: 'any',
-      },
-      {
-        url: '/images/shop-tether-official-logo.jpg',
-        type: 'image/jpg',
-        sizes: '32x32',
-      },
-    ],
-    apple: {
-      url: '/apple-touch-icon.png',
-      sizes: '180x180',
-    },
-  },
-}
 
 export default function RootLayout({
   children,
@@ -44,7 +21,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
-          {!isPasswordPage && <Navbar />}
+          {pathname !== '/password' && <Navbar />}
           {children}
           <Toaster />
         </body>
---
+++ src/app/metadata.ts
@@ -0,0 +1,21 @@
+export const metadata = {
+    title: 'ShopTether - AI-Powered Shopify Services',
+    description: 'Enhance your Shopify store with cutting-edge AI services for personalization, automation, and optimization.',
+    icons: {
+      icon: [
+        {
+          url: '/images/shop-tether-official-logo.jpg',
+          sizes: 'any',
+        },
+        {
+          url: '/images/shop-tether-official-logo.jpg',
+          type: 'image/jpg',
+          sizes: '32x32',
+        },
+      ],
+      apple: {
+        url: '/apple-touch-icon.png',
+        sizes: '180x180',
+      },
+    },
+  }
